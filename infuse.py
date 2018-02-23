fe5213a86429175afa629d2ef9a4e726058cce8a
Alexander Fabisch <alexander.fabisch@dfki.de>
2018-02-23 18:44:06 +0100

    Basic type conversions

--- infuse.py
+++ infuse.py
@@ -1,2 +1,49 @@
+# TODO document
+
+
 def rock2infuse(data):
+    """WARNING: modifies input data!"""
+    data = _translate_typenames(data)
+    data = _translate_types(data)
     return data
+
+
+def _translate_typenames(data):
+    for k in data.keys():
+        if not k.endswith(".meta"):
+            continue
+
+        if "type" in data[k]:
+            data[k]["type"] = _map_typename(data[k]["type"])
+    return data
+
+
+def _map_typename(typename):
+    return typename.split("/")[-1]
+
+
+def _translate_types(data):
+    for k in data.keys():
+        if k.endswith(".meta"):
+            continue
+
+        samples = data[k]
+        for t in range(len(samples)):
+            samples[t] = _translate_sample(samples[t])
+    return data
+
+
+def _translate_sample(sample):
+    if "data" in sample and len(sample) == 1:  # vectors
+        return sample["data"]
+    # TODO don't know how to handle matrices correctly...
+    if "re" in sample and "im" in sample:  # quaternion w, x, y, z
+        return [sample["re"]] + sample["im"]
+
+    if "usecPerSec" in sample:
+        del sample["usecPerSec"]
+
+    for k in sample.keys():
+        if isinstance(sample[k], dict):
+            sample[k] = _translate_sample(sample[k])
+    return sample
--- test/test_infuse.py
+++ test/test_infuse.py
@@ -1,7 +1,76 @@
 from infuse import rock2infuse
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_in
 
 
 def test_convert_nothing():
     data = rock2infuse({})
     assert_equal(data, {})
+
+
+def test_convert_time():
+    data = {
+        "/component.port":
+        [
+            {
+                "microseconds": 5,
+                "usecPerSec": 1000000,
+            }
+        ],
+        "/component.port.meta":
+        {
+            "type": "/base/Time",
+            "timestamps": [5]
+        }
+    }
+    data = rock2infuse(data)
+    assert_in("/component.port", data)
+    samples = data["/component.port"]
+    assert_equal(len(samples), 1)
+    assert_equal(len(samples[0]), 1)
+    assert_equal(samples[0]["microseconds"], 5)
+    assert_in("microseconds", samples[0])
+    assert_in("/component.port.meta", data)
+    meta = data["/component.port.meta"]
+    assert_in("type", meta)
+    assert_equal(meta["type"], "Time")
+    assert_in("timestamps", meta)
+    assert_equal(len(meta["timestamps"]), 1)
+
+
+def test_convert_vector2d():
+    data = {
+        "/component.port":
+        [
+            {
+                "data": [0, 1]
+            }
+        ],
+        "/component.port.meta":
+        {
+            "type": "/base/Vector2d",
+            "timestamps": [5]
+        }
+    }
+    data = rock2infuse(data)
+    samples = data["/component.port"]
+    assert_equal(samples[0], [0, 1])
+
+
+def test_convert_quaternion():
+    data = {
+        "/component.port":
+        [
+            {
+                "re": 0,
+                "im": [1, 2, 3]
+            }
+        ],
+        "/component.port.meta":
+        {
+            "type": "/base/Quaterniond",
+            "timestamps": [5]
+        }
+    }
+    data = rock2infuse(data)
+    samples = data["/component.port"]
+    assert_equal(samples[0], [0, 1, 2, 3])
