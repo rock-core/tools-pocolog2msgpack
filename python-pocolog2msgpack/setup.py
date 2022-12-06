47dd2bc2b9904f3a85f9f5c32cf69fe5db079417
Vinzenz Bargsten <vinzenz.bargsten@dfki.de>
2022-12-06 13:14:16 +0100

    add minimum version requirement of msgpack to setup.py

--- python-pocolog2msgpack/setup.py
+++ python-pocolog2msgpack/setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ "msgpack", "numpy" ]
+requirements = [ "msgpack>=1.0.3", "numpy" ]
 
 test_requirements = [ ]
 
