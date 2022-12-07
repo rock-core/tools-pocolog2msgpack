54093f007c55bd4eda68bbd693d0a4fbbb608f35
Vinzenz Bargsten <vinzenz.bargsten@dfki.de>
2022-12-07 10:18:04 +0100

    use git shallow checkout

--- docker/init.rb
+++ docker/init.rb
@@ -1,2 +1,6 @@
 require 'autoproj/git_server_configuration'
 Autoproj.env_inherit 'CMAKE_PREFIX_PATH'
+
+# don't check out history if possible
+Autobuild::Git.shallow=true
+Autobuild::Git.single_branch=true
